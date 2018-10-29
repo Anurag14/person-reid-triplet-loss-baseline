02094bd7f5f3013562b89a1f11a42219a5d6bf21
Anurag Singh <tellanurag95@gmail.com>
2018-10-29 22:42:52 +0530

    feature extraction

---
+++ feautre_run.sh
@@ -0,0 +1,9 @@
+python script/experiment/fve_extractor.py \
+-d '(0,)' \
+--only_test true \
+--dataset 'market1501' \
+--last_conv_stride 1 \
+--normalize_feature false \
+--exp_dir ~/logs/lastest \
+--model_weight_file ~/logs/latest/ckpt.pth \
+
